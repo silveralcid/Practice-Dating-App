84a7e6f0a65f822e63032c05b71461ff1181dc49
silveralcid <silveralcid@gmail.com>
2024-06-05 17:42:13 -0400

    [49] ng forms

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+  model: any = []
 
+  login() {
+    console.log(this.model);
+  }
 }
