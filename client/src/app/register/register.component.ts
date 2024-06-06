0ac0300a58d2b20f03a1149bfdcd410be474647a
silveralcid <silveralcid@gmail.com>
2024-06-06 09:57:55 -0400

    [50-60] Parent-child communication.

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  @Input() usersFromHomeComponent: any;
+  @Output() cancelRegister = new EventEmitter();
   model: any = {}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
   register() {
     console.log(this.model);
   }
 
+
   cancel() {
-    console.log('cancelled');
+    this.cancelRegister.emit(false);
   }
 }
