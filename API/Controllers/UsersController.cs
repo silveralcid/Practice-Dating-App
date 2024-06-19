fc9668c36e58dbfa36ebff52289683aa39c1b8a0
silveralcid <silveralcid@gmail.com>
2024-06-19 17:41:41 -0400

    [127] Updating users controller, stopped at 1:23.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -15,9 +15,11 @@ public class UsersController : BaseApiController
 {
     private readonly IUserRepository _userRepository;
     private readonly IMapper _mapper;
+    private readonly IPhotoService _photoService;
 
-    public UsersController(IUserRepository userRepository, IMapper mapper)
+    public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
     {
+        _photoService = photoService;
         _mapper = mapper;
         _userRepository = userRepository;
     }
@@ -47,5 +49,11 @@ public class UsersController : BaseApiController
         if (await _userRepository.SaveAllAsync()) return NoContent();
 
         return BadRequest("Failed to update user");
+    }
+
+    [HttpPost("add-photo")]
+    public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+    {
+
     }
 }
