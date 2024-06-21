89038973c9d370077309e24d32625e2510326e06
silveralcid <silveralcid@gmail.com>
2024-06-21 17:25:27 -0400

    [392] Using the Created at Route.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -72,7 +72,11 @@ public class UsersController : BaseApiController
 
         user.Photos.Add(photo);
 
-        if (await _userRepository.SaveAllAsync()) return _mapper.Map<PhotoDto>(photo);
+        if (await _userRepository.SaveAllAsync())
+        {
+            return CreatedAtAction(nameof(GetUser),
+            new { username = user.UserName }, _mapper.Map<PhotoDto>(photo));
+        }
 
         return BadRequest("Problem adding photo");
 
