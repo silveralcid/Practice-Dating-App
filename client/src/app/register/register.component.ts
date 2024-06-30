febb127e194608a682e664aecc89d74a21f6423f
silveralcid <silveralcid@gmail.com>
2024-06-29 20:31:58 -0400

    [403] Reactive Forms intro.

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,10 +12,20 @@ export class RegisterComponent implements OnInit {
 
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
+  registerForm: FormGroup = new FormGroup({});
 
   constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    })
   }
 
   register() {
